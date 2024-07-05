30010893655116aae65e584b14ff7674f381f6f2
KANISHCHOWDARY <potla1508@gmail.com>
2024-07-05 20:09:32 +0530

    Add files via upload

---
+++ Python Problem.py
@@ -0,0 +1,43 @@
+# m=1
+# n=int(input("Enter the number :"))
+# for i in range(1,11):
+#     m=i*n
+#     print(n,"X",i,"=",m)
+# w=float(input("Enter the weight  :"))
+# h=float(input("Enter the height  :"))
+# bmi=w/(h**2)
+# if bmi<18.5:
+#     print("underweight")
+# elif (bmi>=18.5) and (bmi<=24.9):
+#     print("Normalweight")
+# elif (bmi>=25) and (bmi<=29.9):
+#     print("overweight")
+# else:
+#     print("obesity")
+# print("Your BMI is ",bmi)
+k=[]
+while True:
+    c=input(("Enter the value :"))
+    if c=="done":
+        break
+    k.append(c)
+print(k)
+    
+    
+
+
+
+    
+    
+    
+    
+
+
+    
+
+
+
+    
+
+
+

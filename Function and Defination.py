14851aa8b3712b8b69cf8f313a43603675bf721a
KANISHCHOWDARY <potla1508@gmail.com>
2024-07-11 19:59:07 +0530

    Add files via upload

---
+++ Function and Defination.py
@@ -0,0 +1,55 @@
+k=[]
+while True:
+    c=input(("Enter the value :"))
+    if c=="done":
+        break
+    k.append(c)
+print(k)
+def feet():
+    print("Bye world")
+    
+feet()
+d=input("Enter the name :")
+def greet(d):
+    print("Hello",d) 
+
+greet(d)
+def sum(a,b):
+    c=a*b
+    print(c)
+    
+g=int(input("Enter the number 1 :"))
+h=int(input("Enter the number 2 :"))
+sum(g,h)
+
+sum(23,78)
+def eveodd(i):
+    if i%2==0:
+        print("given number is even")
+    else:
+        print("given number is odd")
+        
+i=int(input("Enter the number :"))
+eveodd(i)
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+    

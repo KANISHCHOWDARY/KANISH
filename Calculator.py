f02c72895024cd0d51782a8999447dea3567d5c1
KANISHCHOWDARY <potla1508@gmail.com>
2024-07-18 19:42:04 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+num1=int(input("Enter the first number :"))
+num2=int(input("Enter the second number :"))
+op=input("Enter the operator :")
+if op=="+":
+    print(f"The sum is {num1+num2}")
+elif op=="-":
+    print(f"The subtraction is {num1-num2}")
+elif op=="*":
+    print(f"The multiplication is {num1*num2}")
+elif op=="/":
+    print(f"The division is {num1/num2}")
+else:
+    print("Enter the valid operator")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
